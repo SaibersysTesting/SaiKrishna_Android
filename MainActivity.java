7c8874ee77d8d438a4d21d431ee4168b2be60c85
arun teja <training.saibersys9@gmail.com>
2016-12-26 22:00:06 -0600

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.saikrishna.mynewapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.graphics.Color;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+       RelativeLayout myLayout=new RelativeLayout(this);
+        Button myButton=new Button(this);
+
+
+        myLayout.setBackgroundColor(Color.BLUE);
+        myButton.setBackgroundColor(Color.GREEN);
+        myButton.setText("Click Here");
+
+        RelativeLayout.LayoutParams buttonDetails =
+               new  RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT
+                );
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        myLayout.addView(myButton,buttonDetails);
+
+        EditText username = new EditText(this);
+
+        myButton.setId(1);
+        username.setId(2);
+
+        RelativeLayout.LayoutParams usernameDetails =
+                new  RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT
+                );
+
+        usernameDetails.addRule(RelativeLayout.ABOVE,myButton.getId());
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+        
+
+        usernameDetails.setMargins(0,0,0,50);
+        myLayout.addView(username,usernameDetails);
+
+        setContentView(myLayout);
+
     }
+
+
 }
